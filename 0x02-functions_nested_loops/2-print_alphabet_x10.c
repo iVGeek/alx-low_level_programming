983e3681e9aab1d581c593bfc6526aa55ddda22e
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-13 18:06:51 +0300

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,41 +1,21 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
+ * print_alphabet_x10 - prints alphabet 10 times
  *
- *  * print_alphabet_x10 - prints alphabet 10 times
+ * Return: always 0;
  *
- *   *
- *
- *    * Return: always 0;
- *
- *     *
- *
- *      */
-
+ */
 void print_alphabet_x10(void)
-
 {
+	char i;
+	char h;
 
-		char i;
-
-			char h;
-
-
-
-				for (h = 0; h <= 9; h++)
-
-						{
-
-									for (i = 'a'; i <= 'z'; i++)
-
-												{
-
-																_putchar(i);
-
-																		}
-
-											_putchar('\n');
-
-												}
-
+	for (h = 0; h <= 9; h++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
 }
