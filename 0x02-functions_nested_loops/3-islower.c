8378fa4c383d5c0b8905d2f07e83934ee0e30931
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-13 18:08:27 +0300

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,35 +1,18 @@
 #include "main.h"
-
 /**
+ * _islower - main function
  *
- *  * _islower - main function
+ * @c: character to compare
  *
- *   *
+ * Return: 1 if true, 0 if false
  *
- *    * @c: character to compare
- *
- *     *
- *
- *      * Return: 1 if true, 0 if false
- *
- *       *
- *
- *        */
-
-
+ */
 
 int _islower(int c)
-
 {
-
-		if (c >= 'a' && c <= 'z')
-
-				{
-
-							return (1);
-
-								}
-
-			return (0);
-
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
 }
