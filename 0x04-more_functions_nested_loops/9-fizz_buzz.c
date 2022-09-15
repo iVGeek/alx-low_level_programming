c8b740609c43fd8fa7f4478f8a32d330a5aabfee
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-15 22:31:42 +0300

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,79 +1,40 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Fizz Buzz code
  *
- *  * main - Fizz Buzz code
- *
- *   *
- *
- *    * Return: void
- *
- *     */
-
-
+ * Return: void
+ */
 
 int main(void)
-
 {
+	int i = 1;
 
-		int i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
 
-
-
-			while (i <= 100)
-
-					{
-
-								if (i % 3 == 0 && i % 5 == 0)
-
-											{
-
-															printf("FizzBuzz");
-
-																	}
-
-										else if (i % 3 == 0)
-
-													{
-
-																	printf("Fizz");
-
-																			}
-
-												else if (i % 5 == 0)
-
-															{
-
-																			printf("Buzz");
-
-																					}
-
-														else
-
-																	{
-
-																					printf("%i", i);
-
-																							}
-
-																if (i != 100)
-
-																			{
-
-																							putchar(' ');
-
-																									}
-
-
-
-																		i++;
-
-																			}
-
-				putchar('\n');
-
-					return (0);
-
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
