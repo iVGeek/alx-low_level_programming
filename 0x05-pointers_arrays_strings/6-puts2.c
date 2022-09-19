2644729776ea0e58e79b4e83fe57f512be96fa58
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-19 20:34:10 +0300

    Half the lies they tell about me aren't true
    a function that prints every other character of a string, starting with the first character, followed by a new line.
    
    Prototype: void puts2(char *str);

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,43 +1,22 @@
-i#include "main.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * puts2 - prints xters of a string
- *
- *   * @str: the used string reference
- *
- *    * Return: 0
- *
- *     */
-
-
+ * puts2 - prints xters of a string
+ * @str: the used string reference
+ * Return: 0
+ */
 
 void puts2(char *str)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (str[i] != '\0')
-
-					{
-
-								if (i % 2 == 0)
-
-											{
-
-															_putchar(str[i]);
-
-																	}
-
-										i++;
-
-											}
-
-				_putchar('\n');
-
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
 }
