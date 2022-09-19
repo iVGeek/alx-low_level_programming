9052855a55604634bcae1e09a56e643eb290e4b3
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-19 20:31:05 +0300

    I can only go one way. I've not got a reverse gear
    a function that prints a string, in reverse, followed by a new line.
    
    Prototype: void print_rev(char *s);

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,35 +1,18 @@
-#include "mainn.h"
-
-
+#include "main.h"
 
 /**
- *
- *  * print_rev - prints a string in reverse
- *
- *   * @s: the used string reference pointer
- *
- *    * Return: 0
- *
- *     */
-
-
+ * print_rev - prints a string in reverse
+ * @s: the used string reference pointer
+ * Return: 0
+ */
 
 void print_rev(char *s)
-
 {
+	int i = 0;
 
-		int i = 0;
-
-
-
-			while (s[i])
-
-						i++;
-
-				while (i--)
-
-							_putchar(s[i]);
-
-					_putchar('\n');
-
+	while (s[i])
+		i++;
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
