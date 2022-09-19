675343aeadd0742f6fe22095294086e23c92c725
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-19 20:27:50 +0300

    This report, by its very length, defends itself against the risk of being read
     a function that returns the length of a string.
    
    Prototype: int _strlen(char *s);

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,33 +1,17 @@
-#include "mainn.h"
-
-
+#include "main.h"
 
 /**
+ * _strlen - checks the length of a string
+ * @s: string to check
  *
- *  * _strlen - checks the length of a string
- *
- *   * @s: string to check
- *
- *    *
- *
- *     * Return: int length of the string
- *
- *      */
-
-
+ * Return: int length of the string
+ */
 
 int _strlen(char *s)
-
 {
+	int length = 0;
 
-		int length = 0;
-
-
-
-			while (s[length])
-
-						length++;
-
-				return (length);
-
+	while (s[length])
+		length++;
+	return (length);
 }
