379c18aa1c38e57cd8eb20e49174c023929f5fba
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-26 21:03:17 +0300

    memcpy
     a function that copies memory area.
    
    Prototype: char *_memcpy(char *dest, char *src, unsigned int n);
    The _memcpy() function copies n bytes from memory area src to memory area dest
    Returns a pointer to dest

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,19 +2,16 @@
 
 /**
  * _memcpy - copies memory area
- * @dest: destination of copy
- * @src: memory area to copy
- * @n: number of bytes to copy
- *
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be copied
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	char *ptr = dest;
 
-	for (i = 0; i < n; i++)
-	{
-		*(dest + i) = *(src + i);
-	}
-	return (dest);
+	while (n--)
+		*dest++ = *src++;
+	return (ptr);
 }
