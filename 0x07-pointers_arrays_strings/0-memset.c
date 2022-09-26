0b63b8854f00077d3c49e0eebe8292d4dc257f9c
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-26 21:07:27 +0300

    memset
     a function that fills memory with a constant byte.
    
    Prototype: char *_memset(char *s, char b, unsigned int n);
    The _memset() function fills the first n bytes of the memory area pointed to by s with the constant byte b
    Returns a pointer to the memory area s

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: memory space to be filled
- * @b: byte to fill with
- * @n: number of spaces to fill
+ * _memset - fill memory
+ * @s: input string
+ * @b: input character
+ * @n: input size
  *
- * Return: pointer to memory area s
+ * Description: fills memory with a constant byte
+ * Return: returns a pointer to the memory area s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		s[i] = b;
 	}
 	return (s);
 }
