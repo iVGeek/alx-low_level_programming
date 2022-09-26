6ce2e4ab79f28413f966ddb5ef0a6694a6d61e1d
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-09-26 21:52:52 +0300

    1
    1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,32 @@
-#include "main.h"
-
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area
- * @b: constant byte
- * @n: bytes of the memory area
- * Return: pointer to the memory area s
- */
+ * _memset - a function that fills
+ *         memory with a constant byte
+ *
+ * @s: input pointer to char type
+ *    represents the pointer to the
+ *    block of memory to fill
+ * @b: input variable of char type
+ *    represents the character to
+ *    fill s
+ * @n: unsigned int variable
+ *    the number of bytes to be filled
+ *
+ * Return: A pointer to the filled memory
+ *         area @s
+*/
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *ptr = s;
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	*/
+	unsigned int i;
 
-	while (n--)
-		*s++ = b;
-	return (ptr);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
 }
