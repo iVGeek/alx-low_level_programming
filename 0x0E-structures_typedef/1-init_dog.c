413df73219a2074b33f4c5d0ec0bceee4e02b075
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-10-11 21:15:34 +0300

    A dog is the only thing on earth that loves you more than you love yourself
    a function that initialize a variable of type struct dog
    
    Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+
 /**
- * init_dog - initializes a dog
- * @d: address of dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+ * init_dog - Initializes a variable of type struct dog.
+ * @d: The dog to be initialized.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
