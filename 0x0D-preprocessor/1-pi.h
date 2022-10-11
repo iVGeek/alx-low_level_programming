253fa547bd21b824655a8f559dd024e1135d1705
iVGeek <ianvincent916@gmail.com>
2022-10-11 10:41:02 -0700

    preprocessor

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,23 +1,3 @@
-#ifndef PI_H
-
-#define PI_H
-
-
-
-/*
- *
- *  * File: 1-pi.h
- *
- *   * Desc: Header file that defines a macro named PI
- *
- *    *       as an abbreviation for the token 3.14159265359.
- *
- *     */
-
-
-
+#ifndef PI
 #define PI 3.14159265359
-
-
-
 #endif
