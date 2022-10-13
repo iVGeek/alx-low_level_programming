bc603d8f42d9ea5389968a0e6c11b4d60b496eb5
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-10-13 20:04:57 +0300

    What's my name
    a function that prints a name.
    
    Prototype: void print_name(char *name, void (*f)(char *));

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
-/*
+
+/**
  * print_name - Prints a name.
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
@@ -8,5 +9,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-			f(name);
+
+	f(name);
 }
