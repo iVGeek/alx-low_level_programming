ac361630603ece2e820d9b362ed27a91a00edf08
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-10-27 19:59:54 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,8 +11,10 @@ size_t free_listint_safe(listint_t **h)
 	size_t len = 0;
 	int diff;
 	listint_t *temp;
+
 	if (!h || !*h)
 		return (0);
+
 	while (*h)
 	{
 		diff = *h - (*h)->next;
@@ -29,6 +31,8 @@ size_t free_listint_safe(listint_t **h)
 			break;
 		}
 	}
+
 	*h = NULL;
+
 	return (len);
 }
