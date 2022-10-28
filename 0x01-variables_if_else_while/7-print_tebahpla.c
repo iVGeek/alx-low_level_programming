cb0800998d8ba032a572323cc4c414ef165d4c6f
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-10-28 19:20:01 +0300

    Smile in the mirror
    a program that prints the lowercase alphabet in reverse, followed by a new line.
    
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    All your code should be in the main function
    You can only use putchar twice in your code

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Prints the lowercase alphabet in reverse.
- *
- * Return: Always 0.
- */
+* main - Print the alphabet in lowercase letters in reverse
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char letter;
 
 	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
+	putchar(letter);
 
 	putchar('\n');
 
