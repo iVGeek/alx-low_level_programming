44af8fd77168635637d7ed250fe35135e67e4cb9
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-12-08 19:16:48 +0300

    Update palidrome.py

--- 0x17-doubly_linked_lists/palidrome.py
+++ 0x17-doubly_linked_lists/palidrome.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-
-def check_palindrome(v):
-    reverse = v[::-1]
-
-    if (v == reverse):
-        return True
-    return False
-
-pali = 0
-for i in range(1000):
-    for j in range(1000):
-        if check_palindrome(str(i * j)):
-            pali = i * j
-
-print(pali, end="")
+a = 999
+b = 100
+count = 0;
+palList = []
+for x in range(100,1000):
+    for y in range(100,1000):
+        if(str(x*y) == str(x*y)[::-1]):
+            palList.append(x*y)
+print(max(palList))
