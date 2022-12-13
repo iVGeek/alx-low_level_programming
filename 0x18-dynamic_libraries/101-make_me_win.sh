6e40bbbc6b30cf8de17393f13477ca1cb383c2df
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-12-14 01:52:08 +0300

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-wget -P /tmp https://github.com/iVGeek/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
-
-export LD_PRELOAD=/tmp/libmask.so
+wget https://github.com/svelezg/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
+export LD_PRELOAD=../inject.so
