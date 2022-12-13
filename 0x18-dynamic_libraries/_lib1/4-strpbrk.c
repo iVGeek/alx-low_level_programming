665e5af5afea6d361b8f74f40ba1e195cccd261b
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-12-13 20:13:12 +0300

    4-strpbrk.c

---
+++ 0x18-dynamic_libraries/_lib1/4-strpbrk.c
@@ -0,0 +1,39 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                                               _____  ______    ____  ___  */
+/* 4-strpbrk.c                                  /  _  \ |    |    \   \/  /  */
+/*                                             /  /_\  \|    |     \     /   */
+/* By: iVGeek                                 /    |    \    |___  /     \   */
+/*                                            \____|__  /_______ \/___/\  \  */
+/*                                                    \/        \/      \_/  */
+/*                                                                           */
+/*                                                                           */
+/*****************************************************************************/
+
+#include <string.h>
+
+/**
+ * _strpbrk - a function ...
+ * @s: the chaine
+ * @accept: the chaine
+ *
+ * Return: 1 or 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	if ((s == NULL) || (accept == NULL))
+		return (NULL);
+	while (*s)
+	{
+		if (strchr(accept, *s))
+		{
+			return (s);
+		}
+		else
+		{
+			s++;
+		}
+	}
+	return (NULL);
+}
