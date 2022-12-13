9806f5d7425dd28fb8f8bac0d2640306fa5c91cc
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-12-13 20:04:54 +0300

    3-islower.c

---
+++ 0x18-dynamic_libraries/_lib1/3-islower.c
@@ -0,0 +1,30 @@
+/*
+******************************************************************************
+*                                                                            *
+*                                                 _____  ______    ____  ___ *
+*    5-printf.c                                  /  _  \ |    |    \   \/  / *
+*                                               /  /_\  \|    |     \     /  *
+*    By: iVGeek                                /    |    \    |___  /     \  *
+*                                              \____|__  /_______ \/___/\  \ *
+*                                                      \/        \/      \_/ *
+*                                                                            *
+*                                                                            *
+******************************************************************************
+*/
+
+/**
+ *_islower - a function that checks for lowercase character
+ *@c: The charactere
+ *
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ * Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
